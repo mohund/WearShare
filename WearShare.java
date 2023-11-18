3176f3b56a00289c5716c19fbd02fcba7f9b514b
EssamZarei <EssamZarei@users.noreply.github.com>
2023-11-18 19:25:23 +0300

    First commit

---
+++ WearShare.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+public class WearShare {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int Hi = 66;
+        System.out.println("Hi + " + Hi);
+    }
+}
+
+
---
+++ run.bat
@@ -0,0 +1,3 @@
+@echo off
+javac WearShare.java
+java WearShare
