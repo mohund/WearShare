6f7b53010148a049fec596fc16e4f65e8c9c1cba
EssamZarei <EssamZarei@users.noreply.github.com>
2023-11-18 19:38:30 +0300

    Hi 77

--- WearShare.java
+++ WearShare.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class WearShare {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        int Hi = 66;
+        int Hi = 77;
         System.out.println("Hi + " + Hi);
     }
 }
